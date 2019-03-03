65e9b4308e93dc120d05aae89133a130f24fa7ae
Honohonoho <751718620@qq.com>
2019-03-03 16:08:14 +0800

    优化 热映电影业tabBar

--- src/components/ShowingAndUpcomingMovie.js
+++ src/components/ShowingAndUpcomingMovie.js
@@ -1,33 +1,58 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { 
+    View,
+    Text,
+    StyleSheet,
+    Dimensions
+} from 'react-native';
 import ScrollableTabView, { DefaultTabBar } from 'react-native-scrollable-tab-view';
 
+import OnShowingMovieList from './OnShowingMovie';
 
+const { width, height } = Dimensions.get('window') // 获取设备宽高
 export default class ShowingAndUpcomingMovie extends Component {
     static navigationOptions = {
         header: null
     };
     render() {
         return (
-            <View>
+            <View style={styles.movieListWrap}>
                 <ScrollableTabView
+                    style={{marginTop: 10}}
                     renderTabBar={() => <DefaultTabBar />}
                     tabBarUnderlineStyle={{
-                        backgroundColor: '#000',
-                        height: 2
+                        backgroundColor: '#333',
+                        height: 1,
+                        marginBottom: -1
                     }}
                     tabBarBackgroundColor='#FFFFFF'
-                    tabBarActiveTextColor='#000'
+                    tabBarActiveTextColor='#333'
                     tabBarInactiveTextColor='#959595'
                     tabBarTextStyle={{ fontSize: 14 }}
                     locked={false}
                 >
-                    <View tabLabel='正在热映' style={{ marginBottom: 50 }}>
-                    </View>
-                    <View tabLabel='即将上映' style={{ marginBottom: 50 }}>
+                    <View tabLabel='正在热映' style={styles.tabItemWrap}>
+                        <OnShowingMovieList></OnShowingMovieList>
+                    </View> 
+                    <View tabLabel='即将上映' style={styles.tabItemWrap}>
+                        <Text>即将上映</Text>
                     </View>
                 </ScrollableTabView>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    movieListWrap: {
+        width: width,
+        height: height,
+        backgroundColor: '#ffffff'
+    },
+    tabBarWrap: {
+        height: 50
+    },
+    tabItemWrap: {
+        marginBottom: 100,
+    }
+})
